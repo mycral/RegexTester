1666bde739490206b18db0a711b07456b7d05a02
mycral <guoliulong@gmail.com>
2019-09-15 21:24:55 +0800

    初版

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace RegexTester
+{
+    partial class RegexTextForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.sourceTextBox = new System.Windows.Forms.RichTextBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.testButton = new System.Windows.Forms.Button();
+            this.regexTextBox = new System.Windows.Forms.RichTextBox();
+            this.resultTextBox = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.sourceTextBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 505);
+            this.splitContainer1.SplitterDistance = 181;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // sourceTextBox
+            // 
+            this.sourceTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sourceTextBox.Location = new System.Drawing.Point(12, 12);
+            this.sourceTextBox.Name = "sourceTextBox";
+            this.sourceTextBox.Size = new System.Drawing.Size(776, 166);
+            this.sourceTextBox.TabIndex = 0;
+            this.sourceTextBox.Text = "";
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.testButton);
+            this.splitContainer2.Panel1.Controls.Add(this.regexTextBox);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.resultTextBox);
+            this.splitContainer2.Size = new System.Drawing.Size(800, 320);
+            this.splitContainer2.SplitterDistance = 59;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // testButton
+            // 
+            this.testButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.testButton.Location = new System.Drawing.Point(681, 22);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(75, 23);
+            this.testButton.TabIndex = 1;
+            this.testButton.Text = "测试";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TestButton_MouseClick);
+            // 
+            // regexTextBox
+            // 
+            this.regexTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.regexTextBox.Location = new System.Drawing.Point(12, 3);
+            this.regexTextBox.Name = "regexTextBox";
+            this.regexTextBox.Size = new System.Drawing.Size(618, 53);
+            this.regexTextBox.TabIndex = 0;
+            this.regexTextBox.Text = "";
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultTextBox.Location = new System.Drawing.Point(12, 3);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(776, 242);
+            this.resultTextBox.TabIndex = 0;
+            this.resultTextBox.Text = "";
+            // 
+            // RegexTextForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 505);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Name = "RegexTextForm";
+            this.Text = "正则表达式工具";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox sourceTextBox;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.RichTextBox regexTextBox;
+        private System.Windows.Forms.RichTextBox resultTextBox;
+        private System.Windows.Forms.Button testButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegexTester
+{
+    public partial class RegexTextForm : Form
+    {
+        public RegexTextForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TestButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            string sourceText = sourceTextBox.Text;
+            string regexText = regexTextBox.Text;
+
+            resultTextBox.Text = "";
+
+            try
+            {
+
+                string input = sourceText;
+                string pattern = regexText;
+
+                int matchCount = 0;
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (Match match in Regex.Matches(input, pattern))
+                {
+                    sb.Append(String.Format("第{0}个匹配", matchCount));
+                    sb.Append("\n");
+                    sb.Append(match.Value);
+                    sb.Append("\n");
+
+                    for (int i = 0; i < match.Groups.Count; ++i)
+                    {
+                        sb.Append(String.Format("第{0}子集", i));
+                        sb.Append("\n");
+                        sb.Append(match.Groups[i].Value);
+                        sb.Append("\n");
+                    }
+                }
+                resultTextBox.Text = sb.ToString();
+                int findIndex = sourceTextBox.Find(pattern);
+                if (findIndex != -1)
+                {
+                    sourceTextBox.Focus();
+                    sourceTextBox.Select(findIndex, 1);
+                }
+            }
+            catch (Exception ex)
+            {
+                resultTextBox.Text = ex.ToString();
+            }
+        }
+    }
+}
